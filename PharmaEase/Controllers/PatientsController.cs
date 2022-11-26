61444e9312f7fb9a825be6f95c6d00491e50ce81
Parker Graham <parkerg@shaw.ca>
2022-11-26 15:31:39 -0700

    Patient Page now included although data is still not displayed

---
+++ PharmaEase/Controllers/PatientsController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PharmaEase.Data;
+using PharmaEase.Models;
+
+namespace PharmaEase.Controllers
+{
+    public class PatientsController : Controller
+    {
+        private readonly PharmaEaseContext _context;
+
+        public PatientsController(PharmaEaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Patients
+        public async Task<IActionResult> Index()
+        {
+            var pharmaEaseContext = _context.Patient.Include(p => p.User);
+            return View(await pharmaEaseContext.ToListAsync());
+        }
+
+        // GET: Patients/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null || _context.Patient == null)
+            {
+                return NotFound();
+            }
+
+            var patient = await _context.Patient
+                .Include(p => p.User)
+                .FirstOrDefaultAsync(m => m.GovtHealthNum == id);
+            if (patient == null)
+            {
+                return NotFound();
+            }
+
+            return View(patient);
+        }
+
+        // GET: Patients/Create
+        public IActionResult Create()
+        {
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id");
+            return View();
+        }
+
+        // POST: Patients/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("GovtHealthNum,Fname,Lname,Street,PostalCode,City,Province,UserId")] Patient patient)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(patient);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", patient.UserId);
+            return View(patient);
+        }
+
+        // GET: Patients/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null || _context.Patient == null)
+            {
+                return NotFound();
+            }
+
+            var patient = await _context.Patient.FindAsync(id);
+            if (patient == null)
+            {
+                return NotFound();
+            }
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", patient.UserId);
+            return View(patient);
+        }
+
+        // POST: Patients/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("GovtHealthNum,Fname,Lname,Street,PostalCode,City,Province,UserId")] Patient patient)
+        {
+            if (id != patient.GovtHealthNum)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(patient);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PatientExists(patient.GovtHealthNum))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", patient.UserId);
+            return View(patient);
+        }
+
+        // GET: Patients/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null || _context.Patient == null)
+            {
+                return NotFound();
+            }
+
+            var patient = await _context.Patient
+                .Include(p => p.User)
+                .FirstOrDefaultAsync(m => m.GovtHealthNum == id);
+            if (patient == null)
+            {
+                return NotFound();
+            }
+
+            return View(patient);
+        }
+
+        // POST: Patients/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            if (_context.Patient == null)
+            {
+                return Problem("Entity set 'PharmaEaseContext.Patient'  is null.");
+            }
+            var patient = await _context.Patient.FindAsync(id);
+            if (patient != null)
+            {
+                _context.Patient.Remove(patient);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PatientExists(string id)
+        {
+          return _context.Patient.Any(e => e.GovtHealthNum == id);
+        }
+    }
+}
